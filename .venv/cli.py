f16153405e86b6dd806c82d727f0a71e2e537dab
sklir <sklir@list.ru>
2024-04-27 12:36:06 +0300

    Create basic GUI without interactions, day 16

--- .venv/cli.py
+++ .venv/cli.py
@@ -67,17 +67,3 @@ while True:
         print("Napisal xujniu")
 
 print("Done")
-
-# while True:
-#     ime = input("Vvedi svojo imia: ")
-#     print(ime.capitalize())
-
-# greeting = "hello"
-# print(greeting.upper())
-
-# countries = []
-#
-# while True:
-#     country = input("Enter the country: ")
-#     countries.append(country)
-#     print(countries)
--- .venv/e3.py
+++ .venv/e3.py
@@ -1,3 +1,18 @@
 import shutil
 
 shutil.make_archive("output", 'zip', 'files')
+
+
+# while True:
+#     ime = input("Vvedi svojo imia: ")
+#     print(ime.capitalize())
+
+# greeting = "hello"
+# print(greeting.upper())
+
+# countries = []
+#
+# while True:
+#     country = input("Enter the country: ")
+#     countries.append(country)
+#     print(countries)

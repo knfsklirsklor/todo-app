5581c90e7f2a8f75b308efeb7cd53b222b0334fa
sklir <sklir@list.ru>
2024-04-28 15:38:05 +0300

    Implement the edit button

--- .venv/gui.py
+++ .venv/gui.py
@@ -1,24 +1,41 @@
-import functions
+import functions as ff
 import FreeSimpleGUI as sg
 
 label = sg.Text('Type in a to-do')
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
+input_box = sg.InputText(tooltip="Enter todo", key="dela")
 add_button = sg.Button('Add')
+list_box = sg.Listbox(values=ff.get_todos(), key='items',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button('Edit')
+
 
 window = sg.Window('My To - Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 15))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['items'])
     match event:
         case "Add":
-            todos = functions.get_todos()
-            new_todos = values['todo'] + "\n"
+            todos = ff.get_todos()
+            new_todos = values['dela'] + "\n"
             todos.append(new_todos)
-            functions.write_todos(todos)
+            ff.write_todos(todos)
+            window['items'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['items'][0]
+            new_todo = values['dela']
+
+            todos = ff.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            ff.write_todos(todos)
+            window['items'].update(values=todos)
+        case "items":
+            window['dela'].update(value=values['items'][0])
         case sg.WIN_CLOSED:
             break
 
